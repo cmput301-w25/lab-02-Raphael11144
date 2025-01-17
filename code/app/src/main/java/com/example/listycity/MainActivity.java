99701e7a2858d12aa14c42cc2a675740d301e2a7
Ethan Chen <ethanw714kb@gmail.com>
2025-01-16 22:03:14 -0700

    part2_done

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         cityList = findViewById(R.id.city_list);
-        //Button button_add = findViewById(R.id.button_add);
+        Button button_add = findViewById(R.id.button_add);
+        EditText text_add = findViewById(R.id.editText);
         String[] cities = {"Edmonton", "Paris", "Vancouver"};
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
@@ -45,7 +46,19 @@ public class MainActivity extends AppCompatActivity {
         cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                dataList.remove(position);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
 
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the following line is from chatGpt, "if i have a pain text how
+                // do i get the text in the pain text", 2025/01/16.
+                String cityName = text_add.getText().toString();
+                dataList.add(cityName);
+                cityAdapter.notifyDataSetChanged();
             }
         });
 
